d670e765dea619ec2dcc2e2d05b61c575a8e3375
schwifty <jcmcmanemin@gmail.com>
2017-03-31 16:25:07 -0500

    consolidated main and encrypt pages and added cgi escaping

--- main.py
+++ main.py
@@ -17,32 +17,29 @@ page_footer = """
 </html>
 """
 encrypt_form = """
-      <form action='/encrypt' method='post'>
+      <form action='/' method='post'>
       <label>
-      Rotate by <input type='text' name='caesar'/><label><br>
-      <br>Message to encrypt: <br><textarea rows='4' cols='50' name='message'></textarea><br>
+      Rotate by <input type='text' name='caesar' value='%(caesar)s'/><label><br>
+      <br>Message to encrypt: <br><textarea rows='4' cols='50' name='message'>%(encrypted_message)s</textarea><br>
       <input type='submit' value='Encrypt'/><br>
       </form>
       
       """
 back = "<p><a href='/'><input type='submit' value='Back'/></a></p>"
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-      content = page_header + encrypt_form + page_footer
+    def get(self, encrypted_message = "", caesar = ""):
+      content = page_header + encrypt_form % {'encrypted_message':encrypted_message, 'caesar':caesar} + page_footer
       self.response.write(content)
-    def writeForm(self, caesar='', encrypted_message=''):
-      self.response.out.write(content % {'caesar':caesar,'encrypted_message':encrypted_message})
     
-
-class EncryptedMessage(webapp2.RequestHandler):
     def post(self):
       caesar = self.request.get('caesar')
+      caesar = int(caesar)
       message = self.request.get('message')
       encrypted_message = encrypt(message,caesar)
       encrypted_message = cgi.escape(encrypted_message, quote=True)
-      content = page_header + encrypted_message + back + page_footer
+      content = page_header + encrypt_form % {'encrypted_message':encrypted_message, 'caesar':caesar} + page_footer
       self.response.write(content)
     
 app = webapp2.WSGIApplication([
-    ('/', MainPage),('/encrypt',EncryptedMessage)
+    ('/', MainPage),
 ], debug=True)
